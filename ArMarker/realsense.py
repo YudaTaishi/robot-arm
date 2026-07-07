09d792a925a04d03f35faeab48b89f6cd0a54b11
YudaTaishi <taishi0706yuda@gmail.com>
2026-07-07 22:21:22 +0900

    起動確認

--- ArMarker/realsense.py
+++ ArMarker/realsense.py
@@ -1,13 +1,7 @@
 import pyrealsense2 as rs
 
 ctx = rs.context()
-devices = ctx.query_devices()
+print(ctx.devices.size())
 
-if len(devices) == 0:
-    print("RealSenseが見つかりません")
-else:
-    print(f"{len(devices)} 台見つかりました")
-
-    for dev in devices:
-        print("名前 :", dev.get_info(rs.camera_info.name))
-        print("シリアル :", dev.get_info(rs.camera_info.serial_number))
+for dev in ctx.devices:
+    print(dev.get_info(rs.camera_info.name))
