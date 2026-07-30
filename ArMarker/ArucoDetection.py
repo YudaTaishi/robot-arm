3c445579d0d544e6b3777ebfe01fd1405d695e82
YudaTaishi <taishi0706yuda@gmail.com>
2026-07-30 15:49:29 +0900

    マーカー検出

--- ArMarker/ArucoDetection.py
+++ ArMarker/ArucoDetection.py
@@ -3,19 +3,18 @@ import numpy as np
 import cv2
 
 
-# ==============================
-# RealSense 初期化
-# ==============================
 pipeline = rs.pipeline()
 config = rs.config()
+
 
 config.enable_stream(
     rs.stream.color,
     640,
     480,
-    rs.format.yuyv,
+    rs.format.bgr8,
     30
 )
+
 
 pipeline.start(config)
 
@@ -32,43 +31,24 @@ try:
             continue
 
 
-        # ==============================
-        # YUYVデータ取得
-        # ==============================
-        yuyv = np.asanyarray(color_frame.get_data())
-
-
-        # 確認用
-        # print(yuyv.shape)
-
-
-        # ==============================
-        # YUYV形式をOpenCV用に変換
-        # ==============================
-        if len(yuyv.shape) == 2:
-
-            # (480,640) → (480,640,2)
-            yuyv = yuyv.reshape(
-                (480, 640, 2)
-            )
-
-
-        color_image = cv2.cvtColor(
-            yuyv,
-            cv2.COLOR_YUV2BGR_YUYV
+        color_image = np.asanyarray(
+            color_frame.get_data()
         )
 
 
-        # ==============================
-        # 表示
-        # ==============================
+        print(
+            color_image.shape,
+            color_frame.get_profile().format()
+        )
+
+
         cv2.imshow(
-            "RealSense Color",
+            "Color",
             color_image
         )
 
 
-        if cv2.waitKey(1) & 0xFF == 27:
+        if cv2.waitKey(1) & 0xff == 27:
             break
 
 
