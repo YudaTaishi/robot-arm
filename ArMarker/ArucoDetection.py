d748738bbe336fb5f527fc8c3a3e9eadfd833e8e
YudaTaishi <taishi0706yuda@gmail.com>
2026-07-30 16:17:44 +0900

    マーカ検出

--- ArMarker/ArucoDetection.py
+++ ArMarker/ArucoDetection.py
@@ -7,11 +7,12 @@ pipeline = rs.pipeline()
 config = rs.config()
 
 
+# Color
 config.enable_stream(
     rs.stream.color,
-    640,
+    848,
     480,
-    rs.format.bgr8,
+    rs.format.yuyv,
     30
 )
 
@@ -19,40 +20,38 @@ config.enable_stream(
 pipeline.start(config)
 
 
-try:
+while True:
 
-    while True:
+    frames = pipeline.wait_for_frames()
 
-        frames = pipeline.wait_for_frames()
+    color_frame = frames.get_color_frame()
 
-        color_frame = frames.get_color_frame()
-
-        if not color_frame:
-            continue
+    if not color_frame:
+        continue
 
 
-        color_image = np.asanyarray(
-            color_frame.get_data()
-        )
+    # YUYV画像取得
+    yuyv = np.asanyarray(
+        color_frame.get_data()
+    )
 
 
-        print(
-            color_image.shape,
-            color_frame.get_profile().format()
-        )
+    # YUYV → BGR
+    color_image = cv2.cvtColor(
+        yuyv,
+        cv2.COLOR_YUV2BGR_YUYV
+    )
 
 
-        cv2.imshow(
-            "Color",
-            color_image
-        )
+    cv2.imshow(
+        "RealSense Color",
+        color_image
+    )
 
 
-        if cv2.waitKey(1) & 0xff == 27:
-            break
+    if cv2.waitKey(1) == 27:
+        break
 
 
-finally:
-
-    pipeline.stop()
-    cv2.destroyAllWindows()
+pipeline.stop()
+cv2.destroyAllWindows()
